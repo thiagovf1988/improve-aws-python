3591c5cb36137b6ca8ca77bc10c56880e3ae25c4
thiagovf1988 <61379143+thiagovf1988@users.noreply.github.com>
2020-03-17 21:47:49 +0000

    use click to organize webtron script

---
+++ webtron/webtron.py
@@ -0,0 +1,33 @@
+import boto3
+import click
+
+
+session = boto3.Session(profile_name='Python_Thi')
+s3 = session.resource('s3')
+
+
+@click.group()
+def cli():
+    "Webtron deploy website to aws"
+    pass
+
+
+@cli.command('list-buckets')
+def list_buckets():
+    "List all s3 buckets"
+    for bucket in s3.buckets.all():
+        print(bucket)
+        
+        
+
+@cli.command('list-bucket-objects')
+@click.argument('bucket')
+def list_bucket_objects(bucket):
+    "List objects in an s3 bucket"
+    for obj in s3.Bucket(bucket).objects.all():
+        print(obj)
+    
+
+        
+if __name__ == '__main__':
+    cli()
